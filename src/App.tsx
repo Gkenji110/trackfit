958b679a7e6312028d6e1c20ba66f1100773929c
gkenji110 <gustavo.terada01@gmail.com>
2025-09-10 22:48:09 -0300

    added error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
-import { AddWorkout } from "./pages/add-workout";
-import { WorkoutDetails } from "./pages/workout-details";
-import { NotFound } from "./pages/not-found";
 import { Loading } from "./components/loading";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Fallback } from "./components/fallback";
 
 
-const Home = lazy(() => 
-  import("./pages/home").then(m => ({ default: m.Home })));
+const Home = lazy(() => import("./pages/home").then(m => ({ default: m.Home })));
+const AddWorkout = lazy(() => import('./pages/add-workout').then(m => ({ default: m.AddWorkout })));
+const WorkoutDetails = lazy(() => import('./pages/workout-details').then(m => ({ default: m.WorkoutDetails })));
+const NotFound = lazy(() => import('./pages/not-found').then(m => ({ default: m.NotFound })));
 
 
+function BuggyComponent(){
+  const [crash, setCrash] = useState(false)
+
+  if(crash){
+    throw new Error("erro simulado do componente")
+  }
+
+  return (
+    <button onClick={() => setCrash(true)}>Clique aqui para dar erro</button>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading/>}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/add" element={<AddWorkout />} />
-          <Route path="/workout/:id" element={<WorkoutDetails />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-      </Suspense>
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <Suspense fallback={<Loading/>}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/add" element={<AddWorkout />} />
+              <Route path="/workout/:id" element={<WorkoutDetails />} />
+              <Route path="/bug" element={<BuggyComponent />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </ErrorBoundary> 
     </BrowserRouter>
   );
 }
