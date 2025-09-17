893ae656747a08c281ea56942bef873ced3c63f7
gkenji110 <gusavo.terada01@gmail.com>
2025-09-17 17:10:22 -0300

    feat: created useMemo and useCallback

--- src/components/workout-resume.tsx
+++ src/components/workout-resume.tsx
@@ -3,14 +3,16 @@ import type { Workout } from "../types/workout";
 
 interface WorkoutResumeProps {
   workout: Workout;
+  removeWorkouts: (id:string) => void;
 }
 
-export function WorkoutResume({ workout }: WorkoutResumeProps) {
+export function WorkoutResume({ workout, removeWorkouts }: WorkoutResumeProps) {
   return (
     <div className="w-3/5 rounded-lg shadow px-3 py-4 flex flex-col gap-5 mb-3 bg-white">
       <div className="flex justify-between">
         <h2 className="font-bold text-gray-600">{workout.title}</h2>
 
+        <div className="flex flex-col gap-2">
         <Link
           to={`/workout/${workout.id}`}
           state={{ workout }}
@@ -18,6 +20,10 @@ export function WorkoutResume({ workout }: WorkoutResumeProps) {
         >
           detalhes
         </Link>
+        
+        <button className="bg-red-600 border text-white rounded p-2" onClick={() => removeWorkouts(workout.id)}>Remover treino</button>
+        </div>
+
       </div>
 
       <h3>Data: {workout.date}</h3>
--- src/pages/add-workout.tsx
+++ src/pages/add-workout.tsx
@@ -1,22 +1,65 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { WorkoutForm } from "../components/workout-form";
 import { WorkoutList } from "../components/workout-list";
 import type { Workout } from "../types/workout";
 
+
 export function AddWorkout() {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
 
-  function addWorkout(workout: Workout): void {
-    setWorkouts((prev) => [...prev, workout]);
-  }
+  const workoutTotal = useMemo(() => {
+    return workouts.length
+  }, [workouts])
+  
+  const workoutMinutes = useMemo(() => {
+    let workoutMinutes: number = 0;
+
+    workouts.forEach((workout) => {
+      workoutMinutes += workout.duration;
+    });
+
+    const hours = Math.floor(workoutMinutes / 60)
+    const minutes = workoutMinutes % 60
+
+    return `${hours}: ${minutes}`;
+  } , [workouts])
+  
+    const addWorkout = useCallback((workout: Workout) => {
+      setWorkouts((prev) => [...prev, workout]);
+  }, []);
+
+
+    const removeWorkout = useCallback((id: string) => {
+
+      const index = workouts.findIndex((value) => {
+        return value.id == id
+      });
+
+      const updatedWokouts = [...workouts];
+
+      updatedWokouts.splice(index, 1);
+
+      setWorkouts(updatedWokouts);
+
+    }, [])
 
   return (
     <>
-      <h2 className="font-bold text-blue-800 text-xl mb-3">Adicionar novo treino</h2>
+      <h2 className="font-bold text-blue-800 text-xl mb-3">
+        Adicionar novo treino
+      </h2>
+
+      <p className="text-lf font-bold text-gray-600">
+        Total de treinos: {workoutTotal}
+      </p>
+
+      <p>
+        Tempo de treino: {workoutMinutes} horas
+      </p>
 
       <WorkoutForm onAdd={addWorkout} />
 
-      <WorkoutList workoutList={workouts} />
+      <WorkoutList removeWorkouts={removeWorkout} workoutList={workouts} />
     </>
   );
 }
